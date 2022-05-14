872794fa248ef2fd7b26994cf0fc924def8133f4
Alberto Piu <a@lbr.to>
2022-05-14 02:11:46 +0200

    Add config

--- src/tests/sillabe.test.ts
+++ src/tests/sillabe.test.ts
@@ -2,5 +2,5 @@ import { assertEquals } from "https://deno.land/std@0.139.0/testing/asserts.ts";
 import { VERSION } from "../sillabe.ts";
 
 Deno.test("versiono", () => {
-  assertEquals(VERSION, "0.0.3");
+  assertEquals(VERSION, "0.0.4");
 });
